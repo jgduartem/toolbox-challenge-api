9277203d0027d3b831cd183671d82a262fb55506
Jose Duarte <joseduarte019@gmail.com>
2025-10-04 18:11:38 +0200

    use standard

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
-import express from "express";
-import helmet from "helmet";
-import cors from "cors";
-import FilesRouter from "./routes/filesRouter.js";
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import FilesRouter from './routes/filesRouter.js'
 
-const app = express();
+const app = express()
 
-const port = 8000;
+const port = 8000
 
-app.use(helmet());
+app.use(helmet())
 
 app.use(
   cors({
-    origin: "*",
+    origin: '*'
   })
-);
+)
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/files", new FilesRouter().getRouter());
+app.use('/files', new FilesRouter().getRouter())
 
 app.listen(port, () => {
-  console.log(`TOOLBOX CHALLENGE API RUNNING ON PORT: ${port}`);
-});
+  console.log(`TOOLBOX CHALLENGE API RUNNING ON PORT: ${port}`)
+})
 
-export default app;
+export default app
